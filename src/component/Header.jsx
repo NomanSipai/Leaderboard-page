9a45e1880a5aec96db358d7d34e0c2330059e977
nomanvaghela <nomanvaghela7@gmail.com>
2024-05-27 20:52:09 +0530

    d

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     <div className="sticky z-10 top-0 h-16 border-b bg-white lg:py-2.5">
       <div className="px-6 flex items-center justify-between space-x-4 2xl:container">
-        <h5 hidden="" className="text-2xl text-gray-600 font-medium lg:block">
-          Leaderboard
+        <h5 hidden="" className="text-4xl text-gray-600 font-medium lg:block">
+          🏆
         </h5>
         <button className="w-12 h-16 -mr-2 border-r lg:hidden">
           <svg
