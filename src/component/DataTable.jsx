2e1b508152756083e5e0a43d50119d46827978e2
nomanvaghela <nomanvaghela7@gmail.com>
2024-05-27 21:45:17 +0530

    sds

--- src/component/DataTable.jsx
+++ src/component/DataTable.jsx
@@ -104,8 +104,8 @@ const DataTable = () => {
   return (
     <div>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
             <tr>
               <th scope="col" className="px-6 py-3">
                 Rank
@@ -138,10 +138,10 @@ const DataTable = () => {
               return (
                 <tr
                   key={item.rank}
-                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 text-black font-semibold">
+                  className="bg-white border-b  hover:bg-gray-50 text-black font-semibold">
                   <th
                     scope="row"
-                    className="px-6  py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    className="px-6  py-4 font-medium text-gray-900 whitespace-nowrap">
                     {item.rank}
                   </th>
                   <td className="px-6 py-4 flex items-center text-blue-500 font-bold">
