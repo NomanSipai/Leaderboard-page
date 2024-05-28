3a9d14b49ab2e987c69e633131a9652ee0ef3299
nomanvaghela <nomanvaghela7@gmail.com>
2024-05-28 13:43:47 +0530

    sdsd

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div className="sticky z-10 top-0 h-16 border-b bg-white lg:py-2.5">
       <div className="px-6 flex items-center justify-center space-x-4 2xl:container">
-        <button className="w-12 h-16 -mr-2 border-r lg:hidden">
+        {/* <button className="w-12 h-16 -mr-2 border-r lg:hidden">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6 my-auto"
@@ -16,7 +16,7 @@ const Header = () => {
               d="M4 6h16M4 12h16M4 18h16"
             />
           </svg>
-        </button>
+        </button> */}
 
         <div className="flex space-x-4">
           {/*search bar */}
