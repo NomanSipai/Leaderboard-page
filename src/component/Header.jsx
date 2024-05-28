000f2440813f623c6cca65be4e3f43219b9b02b5
nomanvaghela <nomanvaghela7@gmail.com>
2024-05-28 14:26:07 +0530

    final

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,22 +2,6 @@ const Header = () => {
   return (
     <div className="sticky z-10 top-0 h-16 border-b bg-white lg:py-2.5">
       <div className="px-6 flex items-center justify-end space-x-4 2xl:container">
-        {/* <button className="w-12 h-16 -mr-2 border-r lg:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 my-auto"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </button> */}
-
         <div className="flex space-x-4">
           {/*search bar */}
           <div hidden="" className="md:block">
